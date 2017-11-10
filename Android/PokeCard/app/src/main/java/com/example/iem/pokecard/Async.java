7ce12b2c800fc6e5ac15c3e4ef87b48d01650903
RayanMehdi <rayanmehdi1@gmail.com>
2017-11-10 13:55:37 +0100

    Now able to get Json from API

---
+++ Android/PokeCard/app/src/main/java/com/example/iem/pokecard/Async.java
@@ -0,0 +1,54 @@
+package com.example.iem.pokecard;
+
+import android.os.AsyncTask;
+
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Async extends AsyncTask<Object, Void, String> {
+    TextView tv;
+    @Override
+    protected String doInBackground(Object... params) {
+
+        URL url = null;
+        HttpURLConnection urlConnection = null;
+        BufferedReader in = null;
+        String heure="";
+        tv = (TextView) params[0];
+        try {
+            url = new URL(params[1].toString());
+            urlConnection = (HttpURLConnection) url.openConnection();
+            if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                in = new BufferedReader(
+                        new InputStreamReader(urlConnection.getInputStream(),"UTF-8"));
+                String temp="";
+                while((temp = in.readLine()) != null) {
+                    heure += temp;
+                }
+//ensuite, on récupère le contenu du flux avec in.readLine(), tant qu’il y a des
+//données dans le flux d’entrée
+
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return heure;
+
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        tv.setText(s);
+    }
+}
--- Android/PokeCard/app/src/main/java/com/example/iem/pokecard/MainActivity.java
+++ Android/PokeCard/app/src/main/java/com/example/iem/pokecard/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.iem.pokecard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import java.io.IOException;
+
+import java.net.MalformedURLException;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        final TextView tv = (TextView)findViewById(R.id.textView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Async().execute(tv,"http://pokecardapi.local/index.php/pokemon/list");
+            }
+        });
     }
 }
